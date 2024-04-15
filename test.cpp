4064bc8de5e46fce8e6b3420e069d5e047a701e5
Kate Kovalenko <115945041+spicyte@users.noreply.github.com>
2024-04-15 22:07:02 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void readln() { cout << "readln()\n"; }
+void sort() { cout << "sort()\n"; }
+void compact() { cout << "compact()\n";}
+
+int main()\
+{
+    readln();
+    sort();
+    compact();
+    return 0;
+}
